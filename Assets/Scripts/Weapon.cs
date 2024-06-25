616abffeb56c7f711a31538c2d399e550b5637d0
JacieThoo <s10260236@connect.np.edu.sg>
2024-06-25 14:09:17 +0800

    Added code for idle for orc

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damageAmount = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
