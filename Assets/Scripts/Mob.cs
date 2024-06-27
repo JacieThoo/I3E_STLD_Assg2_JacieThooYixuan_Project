a56f6bf2e5f8029fc0cff4d74c4e5bacc417df89
JacieThoo <s10260236@connect.np.edu.sg>
2024-06-28 00:34:02 +0800

    Update mob script
    Included health bar of mob

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -1,17 +1,75 @@
+/*
+ * Author: Jacie Thoo Yixuan
+ * Date: 06/27/2024
+ * Description: Functions related to mobs, including health bar and death
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mob : MonoBehaviour
-{
-    float healthPoints;
+{   
+    /// <summary>
+    /// Original health of the mob
+    /// </summary>
+    public float maxHealth;
 
-    public void TakeDamage(int damageAmount)
+    /// <summary>
+    /// Current health of the mob
+    /// </summary>
+    private float currentHealth;
+
+    /// <summary>
+    /// Stores the green (mob health left until death) of the health bar
+    /// </summary>
+    public Image healthBarImage;
+
+    /// <summary>
+    /// Update the health bar when the game starts
+    /// </summary>
+    void Start()
     {
-        healthPoints -= damageAmount;
-        if (healthPoints <= 0)
+        // Set original health to max
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    /// <summary>
+    /// Deducts health points from mob and updates the health bar
+    /// Mob dies when health points is 0
+    /// </summary>
+    /// <param name="amount"></param>
+    public void TakeDamage (float damage)
+    {
+        // Deduct health
+        currentHealth -= damage;
+
+        // Update health bar after deducting health
+        UpdateHealthBar ();
+        
+        // When mob dies
+        if (currentHealth <= 0f)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    /// <summary>
+    /// When mob dies, mob object gets destroyed
+    /// </summary>
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// To update health bar according to the current health out of max health
+    /// </summary>
+    void UpdateHealthBar()
+    {
+        float fillAmount = currentHealth / maxHealth;
+        healthBarImage.fillAmount = fillAmount;
     }
 }
