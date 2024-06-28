5d593ab1e914a52037e258530bf684871f74c79d
JacieThoo <s10260236@connect.np.edu.sg>
2024-06-28 16:56:41 +0800

    Changes to scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+
     /// <summary>
     /// The current Interactable of the player
     /// </summary>
