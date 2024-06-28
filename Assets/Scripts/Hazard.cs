f5c6925cf79e2a1c663c34bfff18fe95150ce2b0
JacieThoo <s10260236@connect.np.edu.sg>
2024-06-28 21:30:30 +0800

    Updated hazard script
    Deals damage to the player when collided with

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,18 +1,26 @@
+/*
+ * Author: Jacie Thoo Yixuan
+ * Date: 06/28/2024
+ * Description: Functions related to the hazards
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// The amount of damage the hazard does
+    /// </summary>
+    public float hazardDamage;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Damage the player when player collides with the hazard
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
     {
-        
+        GameManager.Instance.TakeDamage(hazardDamage);
     }
 }
