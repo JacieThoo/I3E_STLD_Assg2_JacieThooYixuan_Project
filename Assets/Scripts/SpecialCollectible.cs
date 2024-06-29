3f70857d88550c57ce503f71dcb0f4b730da3557
JacieThoo <s10260236@connect.np.edu.sg>
2024-06-30 01:32:10 +0800

    Added special collectible script

---
+++ Assets/Scripts/SpecialCollectible.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class SpecialCollectible : Interactable
+{
+    /// <summary>
+    ///  Reference to the BrokenCrystal instance, where the special collectible needs to be placed
+    /// </summary>
+    public BrokenCrystal brokenCrystal;
+    
+    /// <summary>
+    /// Audio for when collectible is collected
+    /// </summary>
+    [SerializeField]
+    private AudioClip collectAudio;
+
+    /// <summary>
+    /// Override Interact()from Interactable base class
+    /// </summary>
+    /// <param name="thePlayer"></param>
+    public override void Interact(Player thePlayer)
+    {
+        // Call the Interact function from the base Interactable class.
+        base.Interact(thePlayer);
+
+        // Call the Collected() function
+        Collected();
+    }
+
+    /// <summary>
+    /// Performs actions related to collection of the collectible
+    /// </summary>
+    public void Collected()
+    {
+        // Play audio at position when collected
+        AudioSource.PlayClipAtPoint(collectAudio, transform.position, 1f);
+
+        // Tells the GameManager that special collectible has been collected
+        GameManager.Instance.CollectSpecialCollectible();
+
+        Debug.Log("Collected special collectible");
+
+        // Destroy the attached GameObject
+        Destroy(gameObject);
+    }
+}
